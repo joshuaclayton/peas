500725740e214a82e81f12eb403b591f3fbd6bfe
Joshua Clayton <jclayton@thoughtbot.com>
2009-12-03 23:10:17 -0500

    Clean up unnecessary tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 ActionController::Routing::Routes.draw do |map|
-  map.root :controller => "high_voltage/pages", :action => "show", :id => "index"
+  map.root :controller => "high_voltage/pages",
+           :action => "show",
+           :id => "index"
   map.resources :peas
   map.peapod "/:pea_key", :controller => "peas", :action => "redir"
 end
