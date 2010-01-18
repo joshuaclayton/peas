31732a78de3f4742df491bc021222256aa469eb2
Joshua Clayton <jclayton@thoughtbot.com>
2010-01-17 21:16:35 -0500

    Update some gems

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -10,8 +10,8 @@ config.action_controller.allow_forgery_protection    = false
 
 config.action_mailer.delivery_method = :test
 
-config.gem "cucumber", :version => ">= 0.4.2"
-config.gem "thoughtbot-shoulda", :lib => "shoulda", :version => ">= 2.10.2"
-config.gem "thoughtbot-factory_girl", :lib => "factory_girl", :version => ">= 1.2.2"
+config.gem "cucumber", :version => ">= 0.6.1"
+config.gem "shoulda", :version => ">= 2.10.3"
+config.gem "factory_girl", :version => ">= 1.2.2"
 config.gem "rspec", :version => ">= 1.2.8", :lib => false
 config.gem "rspec-rails", :version => ">= 1.2.7.1", :lib => false
