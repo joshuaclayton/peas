a03a0f1ef46d6fc6f037c0cf31a0ef41c11c1d0e
Joshua Clayton <jclayton@thoughtbot.com>
2010-01-17 23:00:01 -0500

    Use Shoulda's 'subject' as intended

--- test/unit/pea_test.rb
+++ test/unit/pea_test.rb
@@ -10,12 +10,12 @@ class PeaTest < ActiveSupport::TestCase
   end
 
   context "An unsaved pea" do
-    setup { @pea = Factory.build(:pea) }
+    subject { Factory.build(:pea) }
 
     should "generate a key after it's created" do
-      assert_nil @pea.key
-      @pea.save
-      assert @pea.key.present?
+      assert_nil subject.key
+      subject.save
+      assert subject.key.present?
     end
   end
 end
